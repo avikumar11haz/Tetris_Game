b44d9b5e9801397f2cea4af0d48c8fd3d01da81d
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-10-05 01:08:47 +0530

    create piece and values

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tetris_game/piece.dart';
 import 'package:tetris_game/pixel.dart';
+import 'package:tetris_game/values.dart';
 
 class GameBoard extends StatefulWidget {
   const GameBoard({Key? key}) : super(key: key);
@@ -13,19 +15,43 @@ class _GameBoardState extends State<GameBoard> {
   int rowLength = 10;
   int colLength = 15;
 
+  // current tetris piece
+  Piece currentPiece = Piece(type: Tetromino.L);
+
+  @override
+  void initState() {
+    super.initState();
+
+    // start game when app starts
+    startGame();
+  }
+
+  void startGame() {
+    currentPiece.initializePiece();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: GridView.builder(
-        itemCount: rowLength * colLength,
+          itemCount: rowLength * colLength,
           physics: const NeverScrollableScrollPhysics(),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: rowLength),
-          itemBuilder: (context, index) => Center(
-                child: Pixel(color: Colors.grey[980],
-                child: index,),
-              )),
+          itemBuilder: (context, index) {
+            if(currentPiece.position.contains(index)) {
+              return Pixel(
+                color: Colors.yellow,
+                child: index,
+              );
+            }else {
+              return Pixel(
+                color: Colors.grey[980],
+                child: index,
+              );
+            }
+          }),
     );
   }
 }
